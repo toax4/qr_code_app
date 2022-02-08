8d4009e72723a97978e3e82a7b0ef6a15a161ba6
BOCKELANDT Adrien <a.bockelandt@cosmediterranee.com>
2022-02-08 15:12:23 +0100

    Generate flutter project

---
+++ android/app/src/main/kotlin/fr/apphalla/qrcody/MainActivity.kt
@@ -0,0 +1,6 @@
+package fr.apphalla.qrcody
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/fr/apphalla/qrcodyy/MainActivity.kt
@@ -0,0 +1,6 @@
+package fr.apphalla.qrcodyy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
